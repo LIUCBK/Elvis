d72705ba8f2006decd3d60dc5998db8f11ee8fc0
LIUCBK <liucbk@bc.edu>
2025-01-21 10:50:18 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by 刘佳雨 on 2025/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
